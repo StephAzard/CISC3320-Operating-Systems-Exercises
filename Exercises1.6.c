647a1b9728c98962cd5b0f04270748c801f55a2a
StephAzard <sazard1@yahoo.com>
2026-04-23 17:01:11 -0400

    added omments for reveiw

--- Exercises1.6.c
+++ Exercises1.6.c
@@ -4,7 +4,8 @@ Exercsise 1-6. Verify that the expression getchar() != EOF is 0 or 1.
 #include <stdio.h>
 int main() {
     int c;
-    while((c = getchar()) != EOF) {
-        putchar(c);
+       c = getchar() != EOF;
+       printf("%d\n", c);
+       return 0;
     }
-}
+/* The output is 1 if the input is not EOF and 0 if the input is EOF. */
