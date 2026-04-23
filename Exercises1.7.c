51fe40a23f02ab1129b65cedbaa2f4e027cd6aeb
StephAzard <sazard1@yahoo.com>
2026-04-23 17:06:49 -0400

    print EOF value in C

--- Exercises1.7.c
+++ Exercises1.7.c
@@ -5,3 +5,5 @@ Exercise 1-7. Write a program to print the value of EOF.
 int main() {
 printf("EOF =%d\n", EOF);
 }
+// The value of EOF is 0 or 1 depending on the implementation, 
+//but it is typically -1 in most C implementations.
